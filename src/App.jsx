1d22082bdcbde62be89b5b84b17911837a5fc641
Juan Eulufi Salazar <92868937+BotUsuario@users.noreply.github.com>
2022-11-22 20:38:44 -0300

    ADD: Provider redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import './App.css'
 import { Header } from './components/Header'
 import { Home } from './components/Home'
+import { store } from './store'
+
 
 export const App = () => {
   return (
-    <p className='App'>
+    <Provider store={store}>
+       <div className='App'>
       <Header />
       <Home />
-    </p>
+    </div>
+    </Provider>
+   
   )
 }
